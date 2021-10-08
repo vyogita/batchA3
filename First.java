0f310ba702ed244b1877d96a6dc2e2ca437ca093
vyogita <89368933+vyogita@users.noreply.github.com>
2021-10-08 16:10:31 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+  
+public class First {  
+  
+    public static void main(String[] args) {  
+        
+    // declaration and instantiation of objects/variables  
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");  
+    WebDriver driver=new ChromeDriver();  
+      
+// Launch website  
+    driver.navigate().to("http://www.google.com/");  
+          
+    // Click on the search text box and send value  
+    driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");  
+          
+    // Click on the search button  
+    driver.findElement(By.name("btnK")).click();  
+      
+    }  
+  
+}  
